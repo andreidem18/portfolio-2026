6ec2884c4e7369bdb73923853d82ac99f8c95891
andres <andres.david.mm@hotmail.com>
2026-05-01 21:02:59 -0500

    feat: initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Background } from "@/features/layout/components";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-linear-to-br from-slate-950 via-cyan-950 to-slate-950 overflow-x-hidden">
+      <Background />
+      Hello world
+    </div>
+  );
+}
---
+++ src/features/layout/components/Background.tsx
@@ -0,0 +1,12 @@
+export const Background = () => {
+  return (
+      <div className="fixed inset-0 overflow-hidden pointer-events-none">
+        <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-cyan-500/20 rounded-full blur-3xl animate-pulse"></div>
+        <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-emerald-500/20 rounded-full blur-3xl animate-pulse"></div>
+        <div
+          className="absolute top-1/2 left-1/2 w-96 h-96 bg-blue-500/20 rounded-full blur-3xl animate-pulse"
+          style={{ animationDelay: "1s" }}
+        ></div>
+      </div>
+  )
+}
